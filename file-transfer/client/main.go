914db22cd85df35082593c64155b0f29f96ff434
Darien Raymond <admin@v2ray.com>
2016-11-23 23:05:01 +0100

    file transfer tool

---
+++ file-transfer/client/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"hash/crc32"
+	"io"
+	"net"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+var (
+	file   = flag.String("file", "", "File to transfer.")
+	server = flag.String("server", "", "Server address and port.")
+)
+
+func getHeader() []byte {
+	filename := filepath.Base(*file)
+	header := make([]byte, len(filename)+4)
+	binary.BigEndian.PutUint32(header, uint32(len(filename)))
+	copy(header[4:], []byte(filename))
+	return header
+}
+
+func main() {
+	flag.Parse()
+
+	info, err := os.Stat(*file)
+	if err != nil {
+		fmt.Println("Failed to read file(", *file, "):", err)
+		return
+	}
+	if info.IsDir() {
+		fmt.Println("Unable to transfer directory:", *file)
+		return
+	}
+	fileSize := info.Size()
+	fmt.Println("File name:", *file, "Size:", fileSize/1024/1024, "MB")
+
+	buffer := make([]byte, 1024*1024)
+
+	fmt.Println("Initializing connection.")
+	conn, err := net.Dial("tcp", *server)
+	if err != nil {
+		fmt.Println("Failed to connect to server(", *server, "):", err)
+		return
+	}
+	defer conn.Close()
+
+	if _, err := conn.Write(getHeader()); err != nil {
+		fmt.Println("Failed to send header:", err)
+		return
+	}
+
+	if _, err := conn.Read(buffer); err != nil || buffer[0] != 0 {
+		fmt.Println("Server failed to respond.")
+		return
+	}
+
+	fmt.Println("Begin transfer file. ", time.Now().Format(time.RFC1123Z))
+
+	f, err := os.Open(*file)
+	if err != nil {
+		fmt.Println("Failed to open file:", err)
+		return
+	}
+	defer f.Close()
+
+	totalBytesSent := int64(0)
+	crc := crc32.NewIEEE()
+	for {
+		nBytes, err := f.Read(buffer)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Failed to read file:", err)
+			return
+		}
+		crc.Write(buffer[0:nBytes])
+
+		bytesSent := 0
+		for bytesSent < nBytes {
+			n, err := conn.Write(buffer[bytesSent:nBytes])
+			if err != nil {
+				fmt.Println("Failed to send file:", err)
+				return
+			}
+			bytesSent += n
+		}
+		totalBytesSent += int64(bytesSent)
+	}
+
+	if totalBytesSent != fileSize {
+		fmt.Println("Failed to send whole file. Sent bytes", totalBytesSent, "file bytes", fileSize)
+	}
+
+	fmt.Println("Transfer finished at", time.Now().Format(time.RFC1123Z), "crc", crc.Sum32())
+}
---
+++ file-transfer/server/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"hash/crc32"
+	"io"
+	"net"
+)
+
+var (
+	port = flag.String("port", "", "Port to listen")
+)
+
+func saveFile(conn net.Conn) {
+	defer conn.Close()
+
+	buffer := make([]byte, 1024*1024)
+	nBytes, err := io.ReadAtLeast(conn, buffer, 4)
+	if err != nil {
+		fmt.Println("Failed to read header:", err)
+		return
+	}
+	filenameLen := binary.BigEndian.Uint32(buffer[:4])
+	copy(buffer, buffer[4:nBytes])
+	nBytes -= 4
+	if uint32(nBytes) < filenameLen {
+		_, err := io.ReadFull(conn, buffer[nBytes:filenameLen])
+		if err != nil {
+			fmt.Println("Failed to read header:", err)
+		}
+	}
+	filename := string(buffer[:filenameLen])
+
+	//f, err := os.Create(filename)
+	//if err != nil {
+	//	fmt.Println("Failed to create file:", err)
+	//	conn.Write([]byte{0x01})
+	//	return
+	//}
+	//defer f.Close()
+
+	conn.Write([]byte{0x00})
+
+	fmt.Println("Transfering file:", filename)
+	crc := crc32.NewIEEE()
+	totalBytesReceived := 0
+	for {
+		nBytes, err := conn.Read(buffer)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Failed to read file content:", err)
+			return
+		}
+		totalBytesReceived += nBytes
+		crc.Write(buffer[:nBytes])
+	}
+	fmt.Println("Received", totalBytesReceived, "bytes, crc:", crc.Sum32())
+}
+
+func main() {
+	flag.Parse()
+
+	listerner, err := net.Listen("tcp", *port)
+	if err != nil {
+		fmt.Println("Failed to listen on", *port, ":", err)
+		return
+	}
+
+	for {
+		conn, err := listerner.Accept()
+		if err != nil {
+			fmt.Println("Failed to accept connection:", err)
+			return
+		}
+		go saveFile(conn)
+	}
+}
