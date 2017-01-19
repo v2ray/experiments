14048cde4bee2df2ee48095df491608dee06de5a
Darien Raymond <admin@v2ray.com>
2017-01-19 23:54:12 +0100

    update

--- benchmark/testcases/v2ray_bare.sh
+++ benchmark/testcases/v2ray_bare.sh
@@ -2,7 +2,7 @@
 
 nohup $GOPATH/bin/receiver -port 10001 & PID=$!
 echo "receiver running on $PID"
-nohup /usr/bin/v2ray/v2ray -config=$GOPATH/src/github.com/v2ray/experiements/benchmark/testcases/v2ray_doko_free.json & VPID=$!
+nohup /usr/bin/v2ray/v2ray -config=$GOPATH/src/github.com/v2ray/experiments/benchmark/testcases/v2ray_doko_free.json & VPID=$!
 $GOPATH/bin/loadgen -amount=100
 kill -15 $VPID
 kill -15 $PID
