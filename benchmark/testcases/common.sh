a373a3d956287aa5399a4d77f1c0b6eb572e3c19
Darien Raymond <admin@v2ray.com>
2017-01-20 12:05:18 +0100

    update

--- benchmark/testcases/common.sh
+++ benchmark/testcases/common.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
+PIDS=()
+FINISH=0
+
 function runenv() {
-  PIDS=()
-  PIDS+=($PID)
   for CMD in "$@"; do
     nohup $CMD & PID=$!
     echo "Running componet at p-$PID"
     PIDS+=($PID)
   done
+  {
+    while [ $FINISH -eq 0]; do
+      S=""
+      for PID in "${PIDS[@]}"; do
+        SS="$(ps -p $PID -o pcpu,pmem)"
+        S="$S $SS"
+      done
+      echo "$SS" >> stats.txt
+      sleep 1
+    done
+  }&
+}
+
+function killpids() {
+  FINISH=1
+  for PID in "${PIDS[@]}"; do
+    echo "Killing p-$PID"
+    kill -15 $PID
+  done
+  PIDS=()
 }
--- benchmark/testcases/v2ray_vmess.sh
+++ benchmark/testcases/v2ray_vmess.sh
@@ -7,3 +7,5 @@ source $DIR/common.sh
 runenv "$GOPATH/bin/receiver -port 10001" "/usr/bin/v2ray/v2ray -config=$TEST_DIR/v2ray_doko_vmess.json" "/usr/bin/v2ray/v2ray -config=$TEST_DIR/v2ray_vmess_free.json"
 sleep 2
 $GOPATH/bin/loadgen -amount=10
+
+killpids
