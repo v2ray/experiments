df7e1f67199fd7a563c4f096c5a3c1e7f27d61dc
Darien Raymond <admin@v2ray.com>
2017-01-20 12:12:53 +0100

    update

--- benchmark/testcases/common.sh
+++ benchmark/testcases/common.sh
@@ -4,20 +4,21 @@ PIDS=()
 FINISH=0
 
 function runenv() {
-  rm stats.txt
+  
   for CMD in "$@"; do
     nohup $CMD & PID=$!
     echo "Running componet at p-$PID"
     PIDS+=($PID)
   done
   {
+    rm stats.txt
     while [ $FINISH -eq 0 ]; do
       S=""
       for PID in "${PIDS[@]}"; do
         SS="$(ps -p $PID -o pcpu,pmem --noheader)"
         S="$S $SS"
       done
-      echo "$SS" >> stats.txt
+      echo "$SS" | tr '\n' ' ' | tr ' ' ',' >> stats.txt
       sleep 1
     done
   }&
